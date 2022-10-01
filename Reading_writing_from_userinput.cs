ca4aab92430be8263efcb79f0990e819775aba34
Pooja <poojamarbate090@gmail.com>
2022-10-01 14:57:59 +0530

    taking input from user and display it

---
+++ Reading_writing_from_userinput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reading and writing from user input
+            Console.Write("Enter your name: ");
+
+            string name= Console.ReadLine();
+            //   Console.Write("your name: "+name); //concatention syntax
+
+            Console.WriteLine("your name is :{0}",name);  //placeholder syntax
+            Console.ReadLine();   
+        }
+    }
+}
