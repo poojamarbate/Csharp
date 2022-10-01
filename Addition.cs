db82a0ad9d0a90a6e7b9e417699927d8039b05e0
Pooja <poojamarbate090@gmail.com>
2022-10-01 15:05:37 +0530

    perform addition of two number

---
+++ Addition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine("result:{0}",result);
+            Console.ReadLine();
+            
+        }
+    }
+}
