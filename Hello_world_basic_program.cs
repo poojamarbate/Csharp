902b6c4b171f50b4744b99777a4c5bee468879a9
Pooja <poojamarbate090@gmail.com>
2022-10-01 14:47:07 +0530

    hello world cs file

---
+++ Hello_world_basic_program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* with the help of C# we can build
+              windows application,mobile application,web application ,gaming application
+             
+             C# compile into m/c code.
+
+             dotnet freamwork-----(40 languages avaliable)
+             build libary 
+
+            what is .net freamwork
+            -programming framework created by microsoft
+            -freamwork is just a bunch of code that the programmer can call without having to write it explicity.
+            -collection of libaries
+            -it has number of precoded solution.
+            -a programmer can devlop ap[lications using one of the langauges supoorted by .net
+
+
+             */
+            Console.WriteLine("hello world");
+            Console.ReadKey(); // for holding screen
+
+        }
+    }
+}
