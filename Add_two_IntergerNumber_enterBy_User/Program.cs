4275ddd99358b19e7609a28d4d6d20ff599f2d25
Pooja <poojamarbate090@gmail.com>
2022-10-22 17:23:36 +0530

    addition of two interger number

---
+++ Add_two_IntergerNumber_enterBy_User/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_two_IntergerNumber_enterBy_User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2,result;
+            Console.WriteLine("Enter first number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            result = number1 + number2;
+            Console.WriteLine("Addition :{0} ",result);
+            Console.ReadLine();
+        }
+    }
+}
