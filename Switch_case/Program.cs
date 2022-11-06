ad4a372ffc3eef9a38e0bed1c8dbbcfe81d25721
Pooja <poojamarbate090@gmail.com>
2022-11-06 22:24:17 +0530

    switch case apllication

---
+++ Switch_case/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int week_day = 2;
+            switch (week_day)
+            {
+                case 1: Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wendesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thusday");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+                case 6:
+                    Console.WriteLine("saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("sunday");
+                    break;
+                default: Console.WriteLine("invalid week");
+                    break;
+            }
+            Console.WriteLine("After switch case");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ switch_case_examplw/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            Console.WriteLine("do you want ice-cream ? if yes press '1' OR if NO press '0' .");
+            int num=Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1: Console.WriteLine("Welcome ......do u want which flavor of ice-cream ?");
+                    Console.WriteLine("enter your flavor");
+                    string flavor=Console.ReadLine();
+                    switch (flavor)
+                    {
+                        case "Vanilla":
+                            Console.WriteLine("flavor: Vanilla Ice Creamn");
+                            break;
+
+                        case "Chocolate":
+                            Console.WriteLine("flavor:Chocolate Ice Cream");
+                            break;
+                        case "Strawberry":
+                            Console.WriteLine("flavor:Strawberry Ice Cream");
+                            break;
+                        default:
+                            Console.WriteLine("flavor is not avalible");
+                            break;
+
+                    }
+                    break;
+                case 0: Console.WriteLine("No issue......");
+                    break;
+                default: Console.WriteLine("please rate our service.....hope u like our ice-cream flavor");
+                    break;
+            }
+            Console.WriteLine("BYE BYE.........");
+            Console.ReadLine();
+        }
+    }
+}
