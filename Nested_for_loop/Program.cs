0309408171aee057fb70f2a6d973dd5a59567d0c
Pooja <poojamarbate090@gmail.com>
2022-11-07 17:11:56 +0530

    nested for loop

---
+++ Nested_for_loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("hello");
+                for(int j = 0; j <= 3; j++)
+                {
+                    Console.WriteLine("hey...");
+                }
+            }
+            Console.WriteLine("loop terminate");   //used:multi demisional array
+
+            Console.ReadLine();
+        }
+    }
+}
